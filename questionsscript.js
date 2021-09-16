e85dc1204f2d15478652209c85cd1b5aac938ecd
Josh Elrod <elrodjosh96@gmail.com>
2021-09-16 18:43:18 -0400

    first commit

---
+++ questionsscript.js
@@ -0,0 +1,27 @@
+var timer = document.querySelector("#countdown")
+var startButton = document.querySelector(".start-button")
+
+var timer; 
+var timerCount;
+
+function countdown() {
+    console.log('clicked');
+    var timeLeft = 75;
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft >= 0) {
+            timer.textContent = timeLeft;
+            timeLeft--;
+        }
+        else {
+            timer.textContent = 0;
+            clearInterval(timeInterval);
+        }
+        if(timeLeft=0) {
+            clearInterval()
+        
+        }
+    }, 200);
+}
+
+startButton.addEventListener('click', countdown)

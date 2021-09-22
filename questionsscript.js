5868e9fa95fae24a25d8e9e951e9171e79245352
Josh Elrod <elrodjosh96@gmail.com>
2021-09-22 16:27:42 -0400

    display questions working

--- questionsscript.js
+++ questionsscript.js
@@ -1,12 +1,60 @@
 var timer = document.querySelector("#countdown")
 var startButton = document.querySelector(".start-button")
+var questions = [{
+    question: "What does CSS stand for?",
+    answers: [
+        "Cascading Style Sheets",
+        "Colorful Style Sheets",
+        "Cool Style Sheets"
+        ], 
+    correctAnswer: "Cascading Style Sheets",
+}, {
+    question: "What is the correct syntax for linking a JavaScript file?",
+    answers: [
+        "script",
+        "link.JS",
+        "JS.linkscript"
+        ], 
+    correctAnswer: "script",
+}, {
+    question: "In JavaScript, what is the element called in quotes?",
+    answers: [
+        "array",
+        "string",
+        "boolean"
+        ], 
+    correctAnswer: "string",
+}, {
+    question: "What is a Boolean?",
+    answers: [
+        "string",
+        "array",
+        "true or false"
+        ], 
+    correctAnswer: "true or false",
+}, {
+    question: "How do you write a FOR loop?",
+    answers: [
+        "for (i = 0; i <= 5)",
+        "for (i = 0; i <= 5; i++)",
+        "for (i = 0; i <= 5)"
+        ], 
+    correctAnswer: "for (i = 0; i <= 5; i++)",
+}]
+var questionNum = 0;
 
-var timer; 
+var questionList = document.querySelector("#questions");
+var answerOne = document.querySelector("#answer-one");
+var answerTwo = document.querySelector("#answer-two");
+var answerThree = document.querySelector("#answer-three");
+var scoreTracker = 0;
+
+var timeLeft = 75;
 var timerCount;
 
 function countdown() {
     console.log('clicked');
-    var timeLeft = 75;
+    
 
     var timeInterval = setInterval(function() {
         if (timeLeft >= 0) {
@@ -17,7 +65,56 @@ function countdown() {
             timer.textContent = 0;
             clearInterval(timeInterval);
         }
-    }, 200);
+    }, 1000);
 }
 
-startButton.addEventListener('click', countdown)
+startButton.addEventListener('click', function() {
+    countdown();
+    displayQuestion();
+})
+
+function displayQuestion() {
+    console.log(questions[questionNum].question)
+    questionList.innerHTML = questions[questionNum].question
+    answerOne.innerHTML = questions[questionNum].answers[0]
+    answerTwo.innerHTML = questions[questionNum].answers[1]
+    answerThree.innerHTML = questions[questionNum].answers[2]
+    console.log(questions[questionNum].answers[0])
+    questionNum++;
+    // if statement
+    //got to questions at [i] 
+    //display question
+    //display answers
+    //for loop for answers(answers.length)
+    //event delegation
+    //answer checking
+}
+//for (var i=0; i < questions.length; i++) { 
+
+answerOne.addEventListener('click', function(event) {
+    checkAnswers(event.target.innerHTML);
+    displayQuestion();
+
+})
+answerTwo.addEventListener('click', function(event) {
+    checkAnswers(event.target.innerHTML);
+    displayQuestion();
+    
+})
+answerThree.addEventListener('click', function(event) {
+    checkAnswers(event.target.innerHTML);
+    displayQuestion();
+    
+})
+
+function checkAnswers(userAnswer) {
+    if (questions[questionNum -1].correctAnswer === userAnswer) {
+        scoreTracker++;
+        console.log(scoreTracker)
+    } else {
+        timeLeft = timeLeft -5;
+        timer.textContent = timeLeft;
+        console.log(timeLeft)
+        
+    }
+}
